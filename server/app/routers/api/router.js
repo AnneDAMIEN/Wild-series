6f917d869896f9bff8e8a67bc8d3848d894aaf46
ANNE_DAMIEN <PRO.DAMIENANNE@gmail.com>
2024-05-24 12:45:28 +0200

    Feat and fix : new routers with dynamic path

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+const {sayWelcome} = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
@@ -13,6 +17,10 @@ router.use("/items", itemsRouter);
 const programsRouter = require("./programs/router");
 
 router.use("/programs", programsRouter);
+
+const categoriesRouter = require("./categories/router");
+
+router.use("/categories", categoriesRouter);
 
 /* ************************************************************************* */
 
