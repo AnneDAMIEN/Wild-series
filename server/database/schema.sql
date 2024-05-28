cc667b63f665b934b20aa23afaa9374a5f375570
ANNE_DAMIEN <PRO.DAMIENANNE@gmail.com>
2024-05-28 12:22:36 +0200

    feat: create table category and program

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,19 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table category (
+id int unsigned primary key auto_increment not null,
+name varchar(50) not null
+);
+
+create table program (
+id int unsigned primary key auto_increment not null,
+title varchar(50) not null,
+synopsis varchar(255) not null,
+poster LONGBLOB,
+country varchar(30),
+year int year,
+category_id int unsigned not null,
+foreign key(category_id) references category_id
+);
